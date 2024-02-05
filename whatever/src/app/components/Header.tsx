1cfab1684dac9e64c471aaaad72e4cd1ce475d01
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-02-05 19:05:23 +0900

    파일 구조 만들기

--- whatever/src/app/components/Header.tsx
+++ whatever/src/app/components/Header.tsx
@@ -1,9 +1,16 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
+import Logo from '../../../public/아무거나.svg'
 export default function Header() {
   return (
     <>
-      <Link href='/'>아무거나</Link>
+      <Link href="/">
+        <Image 
+          src={Logo} 
+          alt=''
+          width={196}
+        />
+      </Link>
       <div>메뉴</div>
     </>
   )
