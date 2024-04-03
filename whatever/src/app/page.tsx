17f974baab7713a115a439e9e3a5206c5c94b8d9
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-04-03 17:53:45 +0900

    design: font 파일 추가

--- whatever/src/app/page.tsx
+++ whatever/src/app/page.tsx
@@ -1,3 +1,5 @@
+import Roulette from './components/roulette';
+
 export default function Home() {
   return (
     <main className='absolute top-0 left-0 w-full'>
@@ -5,8 +7,15 @@ export default function Home() {
         <section className='h-3/5  bg-custom-image bg-cover bg-center relative ' >
           <div className ='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50' />
         </section>
-        <section className='h-2/5'>
-        오늘은 뭐 먹지? 더 이상 고민하지 마세요!
+        <section className='h-2/5 bg-stone-200 font-pretendard flex flex-col justify-center items-center text-center'>
+          <p className='text-4xl'>오늘은 뭐 먹지? 더 이상 고민하지 마세요!</p>
+          <article className='flex font-extrabold text-7xl items-center my-8'>
+            <p>오늘은</p>
+            <div className='w-96 h-24 rounded-full shadow-xl mx-8'>
+              <Roulette />
+            </div>
+            <p>먹자!</p>
+          </article>
         </ section>
       </div>
     </main>
