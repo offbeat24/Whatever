ffabab3e327f654ca3d23499af7421d013754ccb
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-04-24 16:54:43 +0900

    feat: foodmap

---
+++ whatever/src/app/random/components/map.tsx
@@ -0,0 +1,5 @@
+export default function FoodMap() {
+  return (
+    <div />
+  )
+}
--- whatever/src/app/random/page.tsx
+++ whatever/src/app/random/page.tsx
@@ -1,12 +1,15 @@
+import FoodMap from './components/map';
+
 export default function RandomPage () {
   return(
     <main className='absolute top-0 left-0 w-full'>
       <div className='flex flex-col h-screen'>
-        <section className='h-[378px]  bg-custom-image bg-cover bg-center relative ' >
-          <div className ='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center'>
-            <p>ㅐㅏㅐㅏㅐㅏㅐㅏ</p>
-          </div>
-        </section>
+        <div className='h-[378px]  bg-custom-image bg-cover bg-center relative flex justify-center items-center' >
+          <div className ='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center' />
+          <section className='w-[1650px] h-[880px] flex justify-center items-center'>
+            <FoodMap />
+          </section>
+        </div>
       </div>
     </main>
   )
