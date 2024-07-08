6600326c5e859e5f95c936f094d3833d988ab0a6
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-07-08 19:07:05 +0900

    style: 데스크톱 메인페이지 작업중

--- whatever/src/app/page.tsx
+++ whatever/src/app/page.tsx
@@ -12,11 +12,14 @@ export default async function Home() {
     <>
       <Header />
       <main className='absolute top-0 left-0 w-full'>
-      <div className='flex flex-col h-screen'>
-        <section className='h-[648px]  bg-custom-image bg-cover bg-center relative ' >
-          <div className ='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex-col justify-center items-start'>
-            <p className='text-4xl text-stone-200'>오늘은 뭐 먹지?</p>
-            <p className='text-4xl text-stone-200'>더 이상 고민하지 마세요!</p>
+        <div className='flex flex-col h-screen'>
+          <section className='h-[67vh]  bg-custom-image bg-cover bg-center relative ' >
+            <div className ='absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex-col justify-center items-start'>
+              <p className='text-4xl text-stone-200'>오늘은 뭐 먹지?</p>
+              <p className='text-4xl text-stone-200'>더 이상 고민하지 마세요!</p>
+            </div>
+          </section>
+          <section className='h-2/5 bg-stone-200 font-pretendard flex flex-col justify-center items-center text-center'>
             <article className='flex-col font-extrabold text-7xl items-start my-8  text-stone-200'>
               <p>오늘은</p>
               <div className='w-[510px] h-24  text-stone-800 bg-stone-200 rounded-full content-around shadow-xl mx-8'>
@@ -24,16 +27,14 @@ export default async function Home() {
               </div>
               <p>먹자!</p>
             </article>
-          </div>
-        </section>
-        <section className='h-2/5 bg-stone-200 font-pretendard flex flex-col justify-center items-center text-center' />
-        <Link href="/random">
-          <section className='flex h-[86px] bg-[#FFA114] justify-center items-center text-center'>
-            <p className='font-pretendard font-extrabold text-3xl'>메뉴 고르기</p>
           </section>
-        </Link>
-      </div>
-    </main>
+          <Link href="/random">
+            <section className='flex h-[86px] bg-[#FFA114] justify-center items-center text-center'>
+              <p className='font-pretendard font-extrabold text-3xl'>메뉴 고르기</p>
+            </section>
+          </Link>
+        </div>
+      </main>
     </>
     
   );
--- whatever/tailwind.config.ts
+++ whatever/tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'mobile': '360px',
+      'tablet': '744px',
+      'laptop' : '1280px'
+    },
     extend: {
       fontFamily: {
         'pretendard': ['PretendardVariable', 'sans-serif']
