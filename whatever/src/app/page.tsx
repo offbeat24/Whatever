023598ee3ae96e8793a1caa039038889871123cd
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-07-12 18:55:06 +0900

    style: 데스크톱 메인페이지 작업중

--- whatever/src/app/page.tsx
+++ whatever/src/app/page.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import usedCSVData from '../hooks/usedCSVData';
 import Roulette from './components/roulette';
 import Header from './Header/header';
+import DLogo from '../../public/3D_Logo.png'
 
 export default async function Home() {
 
@@ -13,13 +15,18 @@ export default async function Home() {
       <main className='absolute top-0 left-0 w-full'>
         <div className='flex flex-col h-screen'>
           <section className='h-[67vh]  bg-custom-image bg-cover bg-center relative ' >
-            <div className ='absolute top-0 left-0 w-full h-full text-[4.375rem] font-extrabold bg-black bg-opacity-50 flex-col justify-center items-start'>
-              <p className='text-white'>오늘은 뭐 먹지?</p>
-              <p className='text-white'>더 이상 고민하지 마세요!</p>
+            <div className ='w-full h-full text-[4.375rem] font-extrabold bg-black bg-opacity-50'>
+              <div className='flex justify-center my-auto'>
+                <div className='flex-col my-auto'> 
+                  <p className='text-white'>오늘은 뭐 먹지?</p>
+                  <p className='text-white'>더 이상 고민하지 마세요!</p>
+                </div>
+                <Image src={DLogo} alt='' className='w-[254px]'/>
+              </div>
             </div>
           </section>
           <section className='h-2/5 bg-white font-pretendard flex flex-col justify-center items-center text-center'>
-            <article className='flex-col  text-black font-extrabold text-7xl items-start my-8  '>
+            <article className='flex text-black font-extrabold text-7xl items-start my-8 align-middle'>
               <p>오늘은</p>
               <div className='w-[510px] h-24  bg-white rounded-full content-around shadow-xl mx-8'>
                 <Roulette textData={foodList}/>
