39a96bde8b017b23722f43c1c8383ca02733b056
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-07-18 19:40:27 +0900

    style:네비게이션 작업

--- whatever/src/app/random/components/gnb.tsx
+++ whatever/src/app/random/components/gnb.tsx
@@ -1,8 +1,14 @@
 export default function Navigation() {
   return (
-    <div className="fixed top-0 left-0 h-full w-64 bg-white shadow-lg p-4 z-10">
-      <h2>사이드바 내용</h2>
-      <p>이곳에 사이드바 내용을 추가하세요</p>
+    <div className="fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[4.6875rem] laptop:bg-orange-o3 laptop:shadow-lg laptop:p-4 laptop:z-10
+    bottom-0 left-0 w-full h-16 bg-orange-o3 shadow-lg p-4 z-10 flex laptop:flex-col justify-center items-center">
+      <h2 className="laptop:block hidden">사이드바 내용</h2>
+      <p className="laptop:block hidden">이곳에 사이드바 내용을 추가하세요</p>
+      <div className="laptop:hidden flex justify-around w-full">
+        <span>홈</span>
+        <span>메뉴</span>
+        <span>설정</span>
+      </div>
     </div>
   );
 }
--- whatever/src/app/random/page.tsx
+++ whatever/src/app/random/page.tsx
@@ -3,9 +3,9 @@ import Navigation from './components/gnb';
 
 export default function RandomPage () {
   return(
-    <section className='fixed inset-0 flex items-center justify-center'>
+    <section className='fixed inset-0 flex flex-col laptop:flex-row'>
       <Navigation />
-      <div className='flex-grow' >
+      <div className='flex-grow laptop:ml-[4.6875rem]' >
         <FoodMap />
       </div>
     </section>
