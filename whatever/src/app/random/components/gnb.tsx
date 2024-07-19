bc6fefaf08d080e53d0cd07cd063e9a624abdce5
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-07-19 17:48:01 +0900

    style:랩톱 버전 네비게이션바 디자인

--- whatever/src/app/random/components/gnb.tsx
+++ whatever/src/app/random/components/gnb.tsx
@@ -1,15 +1,58 @@
+import Image from 'next/image';
+
 export default function Navigation() {
   return (
-    <div className="fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[4.6875rem] laptop:bg-orange-o3 laptop:shadow-lg laptop:p-4 laptop:z-10
-    bottom-0 left-0 w-full h-16 bg-orange-o3 shadow-lg p-4 z-10 flex laptop:flex-col justify-center items-center">
-      <h2 className="laptop:block hidden">사이드바 내용</h2>
-      <p className="laptop:block hidden">이곳에 사이드바 내용을 추가하세요</p>
-      <div className="laptop:hidden flex justify-around w-full">
-        <span>홈</span>
-        <span>메뉴</span>
-        <span>설정</span>
+    <nav className="fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[4.6875rem] laptop:bg-orange-o3 laptop:shadow-[15px_0px_25px_0px_rgba(0,0,0,0.15)] laptop:z-10
+    bottom-0 left-0 w-full h-16 bg-orange-o3 z-10 flex laptop:flex-col justify-start items-center">
+      <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
+        <div className='laptop:w-[3.125rem]'>
+          <Image
+            src='/logo_vertical.svg'
+            alt="logo"
+            layout="responsive"
+            width={50}
+            height={32}
+            className="object-contain w-full"
+          />
+        </div>
       </div>
-    </div>
+      <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
+        <div className='laptop:w-11'>
+          <Image
+            src='/SearchText.svg'
+            alt="search"
+            layout="responsive"
+            width={44}
+            height={44}
+            className="object-contain w-full"
+          />
+        </div>
+      </div>
+      <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
+        <div className='laptop:w-11'>
+          <Image
+            src='/Bookmark.svg'
+            alt="bookmark"
+            layout="responsive"
+            width={44}
+            height={44}
+            className="object-contain w-full"
+          />
+        </div>
+      </div>
+      <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
+        <div className='laptop:w-[2.625rem]'>
+          <Image
+            src='/HistoryText.svg'
+            alt="history"
+            layout="responsive"
+            width={41}
+            height={45}
+            className="object-contain w-full"
+          />
+        </div>
+      </div>
+    </nav>
   );
 }
 
