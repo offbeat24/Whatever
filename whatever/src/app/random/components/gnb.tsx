26bf832d1f0faa771875342f88ed90964767f295
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-07-22 17:51:42 +0900

    style: 반응형 ui

--- whatever/src/app/random/components/gnb.tsx
+++ whatever/src/app/random/components/gnb.tsx
@@ -22,9 +22,10 @@ export default function Navigation() {
     closed: { y: '100%' },
   }
   return (
-    <nav className="fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[4.6875rem] laptop:bg-orange-o3 laptop:shadow-[15px_0px_25px_0px_rgba(0,0,0,0.15)] laptop:z-10
-    bottom-0 left-0 w-full h-16 bg-orange-o3 z-10 flex laptop:flex-col justify-start items-center">
-      <Link href="/">
+    <nav className="fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[4.6875rem] laptop:shadow-[15px_0px_25px_0px_rgba(0,0,0,0.15)] laptop:flex-col laptop:justify-start
+    tablet:h-3.125rem tablet:[filter:drop-shadow(2px_2px_10px_rgba(0,0,0,0.25))]
+    flex-row bottom-0 left-0 w-full bg-orange-o3 z-10 flex justify-between items-center">
+      <Link href="/" className='hidden laptop:block'>
         <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
           <div className='laptop:w-[3.125rem]'>
             <Image
@@ -38,8 +39,12 @@ export default function Navigation() {
           </div>
         </div>
       </Link>
-      <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
-        <div className='laptop:w-11'>
+      <button
+        type="button"
+        className={`flex laptop:w-[4.6875rem] laptop:h-[5.9375rem] w-full laptop:border-b-neutral-50 laptop:border-b laptop:border-r-0 border-r-neutral-50 border-r border-solid justify-center items-center cursor-pointer ${activeMenu === 'search' ? 'bg-fafafa' : ''}`}
+        onClick={() => handleMenuClick('search')}
+      >
+        <div className={`laptop:w-11 tablet:w-[2.25rem] ${activeMenu === 'search' ? 'text-orange-o3' : ''}`}>
           <Image
             src='/SearchText.svg'
             alt="search"
@@ -49,9 +54,13 @@ export default function Navigation() {
             className="object-contain w-full"
           />
         </div>
-      </div>
-      <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
-        <div className='laptop:w-11'>
+      </button>
+      <button
+        type="button"
+        className={`flex laptop:w-[4.6875rem] laptop:h-[5.9375rem] w-full laptop:border-b-neutral-50 laptop:border-b laptop:border-r-0 border-r-neutral-50 border-r border-solid justify-center items-center cursor-pointer ${activeMenu === 'bookmark' ? 'bg-fafafa' : ''}`}
+        onClick={() => handleMenuClick('bookmark')}
+      >
+        <div className={`laptop:w-11 tablet:w-[2.25rem] ${activeMenu === 'bookmark' ? 'text-orange-o3' : ''}`}>
           <Image
             src='/Bookmark.svg'
             alt="bookmark"
@@ -61,9 +70,13 @@ export default function Navigation() {
             className="object-contain w-full"
           />
         </div>
-      </div>
-      <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
-        <div className='laptop:w-[2.625rem]'>
+      </button>
+      <button
+        type="button"
+        className={`flex laptop:w-[4.6875rem] laptop:h-[5.9375rem] w-full laptop:border-b-neutral-50 laptop:border-b laptop:border-r-0 border-solid justify-center items-center cursor-pointer ${activeMenu === 'history' ? 'bg-fafafa' : ''}`}
+        onClick={() => handleMenuClick('history')}
+      >
+        <div className={`laptop:w-[2.625rem] tablet:w-[2.25rem] ${activeMenu === 'history' ? 'text-orange-o3' : ''}`}>
           <Image
             src='/HistoryText.svg'
             alt="history"
@@ -73,7 +86,51 @@ export default function Navigation() {
             className="object-contain w-full"
           />
         </div>
-      </div>
+      </button>
+
+      <AnimatePresence>
+        {
+          activeMenu && (
+            <>
+            {/* {사이드바 (laptop)} */}
+            <motion.div
+              initial="closed"
+              animate="open"
+              exit="closed"
+              variants={sidebarVariants}
+              transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+              className="fixed top-0 left-0 w-[300px] h-full bg-white shadow-lg z-20 hidden laptop:block"
+              >
+                <button
+                  type="button"
+                  onClick={()=> setActiveMenu(null)} className="absolute top-4 right-4">
+                    Close
+                </button>
+                {activeMenu === 'search' && <div>Search Content</div>}
+                {activeMenu === 'bookmark' && <div>Bookmark Content</div>}
+                {activeMenu === 'history' && <div>History Content</div>}  
+              </motion.div>
+
+              {/* 하단 네비게이션 (모바일 및 태블릿) */}
+              <motion.div
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={bottomNavVariants}
+                transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+                className="fixed bottom-0 left-0 w-full h-[300px] bg-white shadow-lg z-20 block laptop:hidden"
+              >
+                <button type="button" onClick={() => setActiveMenu(null)} className="absolute top-4 right-4">
+                  Close
+                </button>
+                {activeMenu === 'search' && <div>Search Content</div>}
+                {activeMenu === 'bookmark' && <div>Bookmark Content</div>}
+                {activeMenu === 'history' && <div>History Content</div>}
+              </motion.div>
+            </>
+          )
+        }
+      </AnimatePresence>
     </nav>
   );
 }
