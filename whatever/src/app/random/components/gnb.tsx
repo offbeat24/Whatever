05323f4a7b60b74149c6a2b89ac586cbde4ef316
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2024-07-23 19:40:01 +0900

    style: gnb ui 적용완료

--- whatever/src/app/random/components/gnb.tsx
+++ whatever/src/app/random/components/gnb.tsx
@@ -12,6 +12,10 @@ export default function Navigation() {
     setActiveMenu(menu === activeMenu ? null : menu);
   };
 
+  const closeMenu = () => {
+    setActiveMenu(null);
+  };
+
   const sidebarVariants = {
     open: { x: 0 },
     closed: { x: '-100%' },
@@ -22,84 +26,27 @@ export default function Navigation() {
     closed: { y: '100%' },
   }
   return (
-    <nav className="fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[4.6875rem] laptop:shadow-[15px_0px_25px_0px_rgba(0,0,0,0.15)] laptop:flex-col laptop:justify-start
-    tablet:h-3.125rem tablet:[filter:drop-shadow(2px_2px_10px_rgba(0,0,0,0.25))]
-    flex-row bottom-0 left-0 w-full bg-orange-o3 z-30 flex justify-between items-center">
-      <Link href="/" className='hidden laptop:block'>
-        <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-neutral-50 border-b border-solid justify-center items-center'>
-          <div className='laptop:w-[3.125rem]'>
-            <Image
-              src='/logo_vertical.svg'
-              alt="logo"
-              layout="responsive"
-              width={50}
-              height={32}
-              className="object-contain w-full"
-            />
-          </div>
-        </div>
-      </Link>
-      <button
-        type="button"
-        className={`flex laptop:w-[4.6875rem] laptop:h-[5.9375rem] w-full laptop:border-b-neutral-50 laptop:border-b laptop:border-r-0 border-r-neutral-50 border-r border-solid justify-center items-center cursor-pointer ${activeMenu === 'search' ? 'bg-white' : ''}`}
-        onClick={() => handleMenuClick('search')}
-      >
-        <div className={`laptop:w-11 tablet:w-[2.25rem] ${activeMenu === 'search' ? 'text-orange-o3' : ''}`}>
-          <Image
-            src='/SearchText.svg'
-            alt="search"
-            layout="responsive"
-            width={44}
-            height={44}
-            className="object-contain w-full"
-          />
-        </div>
-      </button>
-      <button
-        type="button"
-        className={`flex laptop:w-[4.6875rem] laptop:h-[5.9375rem] w-full laptop:border-b-neutral-50 laptop:border-b laptop:border-r-0 border-r-neutral-50 border-r border-solid justify-center items-center cursor-pointer ${activeMenu === 'bookmark' ? 'bg-fafafa' : ''}`}
-        onClick={() => handleMenuClick('bookmark')}
-      >
-        <div className={`laptop:w-11 tablet:w-[2.25rem] ${activeMenu === 'bookmark' ? 'text-orange-o3' : ''}`}>
-          <Image
-            src='/Bookmark.svg'
-            alt="bookmark"
-            layout="responsive"
-            width={44}
-            height={44}
-            className="object-contain w-full"
-          />
-        </div>
-      </button>
-      <button
-        type="button"
-        className={`flex laptop:w-[4.6875rem] laptop:h-[5.9375rem] w-full laptop:border-b-neutral-50 laptop:border-b laptop:border-r-0 border-solid justify-center items-center cursor-pointer ${activeMenu === 'history' ? 'bg-fafafa' : ''}`}
-        onClick={() => handleMenuClick('history')}
-      >
-        <div className={`laptop:w-[2.625rem] tablet:w-[2.25rem] ${activeMenu === 'history' ? 'text-orange-o3' : ''}`}>
-          <Image
-            src='/HistoryText.svg'
-            alt="history"
-            layout="responsive"
-            width={41}
-            height={45}
-            className="object-contain w-full"
-          />
-        </div>
-      </button>
-
-      <AnimatePresence>
-        {
-          activeMenu && (
+    <div className="relative">
+      {/* 메뉴 컨테이너 */}
+      <div className="fixed inset-0 z-20 pointer-events-none">
+        <AnimatePresence>
+          {activeMenu && (
             <>
-            {/* {사이드바 (laptop)} */}
-            <motion.div
-              initial="closed"
-              animate="open"
-              exit="closed"
-              variants={sidebarVariants}
-              transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-              className="fixed top-0 left-[4.5rem] w-96 h-full z-10 bg-white shadow-lg hidden laptop:block"
+              <motion.div
+                className="fixed inset-0 bg-black bg-opacity-50 pointer-events-auto "
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                onClick={closeMenu}
+              />
+              {/* 사이드바 (laptop) */}
+              <motion.div
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={sidebarVariants}
+                transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+                className="fixed top-0 left-[4.5rem] w-96 h-full bg-white hidden laptop:block pointer-events-auto shadow-[15px_0px_25px_0px_rgba(0,0,0,0.15)]"
               >
                 <button
                   type="button"
@@ -118,7 +65,7 @@ export default function Navigation() {
                 exit="closed"
                 variants={bottomNavVariants}
                 transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-                className="fixed bottom-0 left-0 w-full h-[300px] z-10 bg-white shadow-lg block laptop:hidden"
+                className="fixed bottom-0 left-0 w-full h-[300px] bg-white shadow-[0px_-15px_14px_0px_rgba(0,0,0,0.15)] block laptop:hidden pointer-events-auto"
               >
                 <button type="button" onClick={() => setActiveMenu(null)} className="absolute top-4 right-4">
                   Close
@@ -128,10 +75,49 @@ export default function Navigation() {
                 {activeMenu === 'history' && <div>History Content</div>}
               </motion.div>
             </>
-          )
-        }
-      </AnimatePresence>
-    </nav>
+          )}
+        </AnimatePresence>
+      </div>
+
+      {/* 네비게이션 */}
+      <nav className={`fixed laptop:top-0 laptop:left-0 laptop:h-full laptop:w-[4.6875rem] ${!activeMenu ? 'laptop:shadow-[15px_0px_25px_0px_rgba(0,0,0,0.15)]' : ''} laptop:flex-col laptop:justify-start
+      h-[3.125rem] ${!activeMenu ? 'tablet:shadow-[0px_-15px_14px_0px_rgba(0,0,0,0.15)]' : ''}
+      flex-row bottom-0 left-0 w-full bg-orange-o3 z-30 flex justify-between items-center`}>
+        <Link href="/" className='hidden laptop:block'>
+        <div className='flex laptop:w-[4.6975rem] laptop:h-[5.9375rem] border-b-white border-b border-solid justify-center items-center'>
+          <div className='laptop:w-[3.125rem]'>
+            <Image
+              src='/logo_vertical.svg'
+              alt="logo"
+              layout="responsive"
+              width={50}
+              height={32}
+              className="object-contain w-full"
+            />
+          </div>
+        </div>
+      </Link>
+      {['search', 'bookmark', 'history'].map((menu) => (
+        <button
+          key={menu}
+          type="button"
+          className={`flex laptop:h-[5.9375rem] w-full h-full laptop:border-b-white laptop:border-b border-solid justify-center items-center cursor-pointer ${activeMenu === menu ? 'bg-white' : ''}`}
+          onClick={() => handleMenuClick(menu)}
+        >
+          <div className={`laptop:w-11 tablet:w-[2.25rem] ${activeMenu === menu ? 'text-orange-o3' : ''}`}>
+            <Image
+              src={activeMenu === menu ? `/${`${menu.charAt(0).toUpperCase() + menu.slice(1) }Text`}_activate.svg` : `/${`${menu.charAt(0).toUpperCase() + menu.slice(1) }Text`}.svg`}
+              alt={menu}
+              layout="responsive"
+              width={44}
+              height={44}
+              className="object-contain w-full"
+            />
+          </div>
+        </button>
+      ))}
+      </nav>
+    </div>
   );
 }
 
